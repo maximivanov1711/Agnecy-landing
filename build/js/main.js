258c78bbd820bf0aa0ad2f7e7b220660cd86f83b
maximivanov2004 <maxim.ivanov1711@gmail.com>
2020-04-24 22:34:42 +0300

    commit-9: js

--- build/js/main.js
+++ build/js/main.js
@@ -1,1 +1,1 @@
-$(document).ready((function(){$("#menu-button").on("click",(function(){$("#menu").toggleClass("active"),$("header").toggleClass("active")}))}));
+$(document).ready((function(){$("#menu-button").on("click",(function(){$("#menu").toggleClass("active"),$("header").toggleClass("active")})),$(".gallery__photo").on("click",(function(){let t=$(this).attr("data-path");$("#photo-big").attr("src",t)}))}));
--- src/js/main.js
+++ src/js/main.js
@@ -3,4 +3,9 @@ $(document).ready(function(){
     $("#menu").toggleClass("active");
     $("header").toggleClass("active");
   });
+
+  $(".gallery__photo").on("click", function(){
+    let path = $(this).attr("data-path");
+    $("#photo-big").attr("src", path);
+  });
 });
